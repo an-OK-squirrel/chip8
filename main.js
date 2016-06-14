be72990a16fcbd833323a57691e92f62f13f5b21
an-OK-squirrel <an-OK-squirrel@users.noreply.github.com>
2016-06-14 16:18:45 -0500

    I don't even remember what I did this time.

--- main.js
+++ main.js
@@ -1,10 +1,6 @@
 function Chip8() {
     this.memory = new Uint8Array(4096); // General RAM
     this.registers = new Unit8Array(8); // registers
-    this.programCounter = 200;
-    this.indexRegister = 0;
-    this.delayTimer = 0;
-    this.soundTimer = 0;
 }
 
 Chip8.prototype.reset() = function() {
@@ -15,4 +11,8 @@ Chip8.prototype.reset() = function() {
     this.indexRegister = 0;
     this.memory.fill(0);
     this.registers.fill(0)
+}
+
+Chip8.prototype.loadProgram() = function(program) { // program is Unit8array
+    program.forEach((value, index, array) => {this.memory[0x400 + index] = value})
 }
